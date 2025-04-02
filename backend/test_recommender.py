82a6d35e6866a13e5a54565324ea648d2e0ff02f
Teemu <teemu_salonen2@hotmail.fi>
2025-04-02 16:07:32 +0300

    add args

--- backend/test_recommender.py
+++ backend/test_recommender.py
@@ -6,16 +6,33 @@ if __name__ == "__main__":
     parser.add_argument("--dataset", type=str, default="train", help="Dataset type: train or valid")
     parser.add_argument("--cluster_id", type=str, default=None,
                         help="Cluster ID (or comma-separated list) to evaluate (if not provided, all clusters will be processed)")
+    parser.add_argument("--dataset_size", type=str, default="large", help="Dataset size: large or small")
     parser.add_argument("--process_dfs", action='store_true', help="Process dataframes if needed")
     parser.add_argument("--process_behaviors", action='store_true', help="Process behaviors if needed")
     args = parser.parse_args()
+     if args.dataset_size == "small":
+         data_dir_train="dataset/small/train/"
+         data_dir_valid="dataset/small/valid/"
+         zip_file_train="MINDsmall_train.zip"
+         zip_file_valid="MINDsmall_dev.zip"
+         user_category_profiles_path="small_user_category_profiles.pkl"
+         user_cluster_df_path="small_user_cluster_df.pkl"
+         cluster_id=args.cluster_id
+     else:
+         data_dir_train="dataset/train/"
+         data_dir_valid="dataset/valid/"
+         zip_file_train="MINDlarge_train.zip"
+         zip_file_valid="MINDlarge_dev.zip"
+         user_category_profiles_path="user_category_profiles.pkl"
+         user_cluster_df_path="user_cluster_df.pkl"
+         cluster_id=args.cluster_id
 
     main(dataset=args.dataset,
          process_dfs=args.process_dfs, process_behaviors=args.process_behaviors,
-         data_dir_train="dataset/small/train/", data_dir_valid="dataset/small/valid/",
-         zip_file_train="MINDsmall_train.zip", zip_file_valid="MINDsmall_dev.zip",
-         user_category_profiles_path="small_user_category_profiles.pkl",
-         user_cluster_df_path="small_user_cluster_df.pkl", cluster_id=args.cluster_id)
+         data_dir_train=data_dir_train, data_dir_valid=data_dir_valid,
+         zip_file_train=zip_file_train, zip_file_valid=zip_file_valid,
+         user_category_profiles_path=user_category_profiles_path,
+         user_cluster_df_path=user_cluster_df_path, cluster_id=args.cluster_id)
     """main(dataset=args.dataset,
          process_dfs=args.process_dfs, process_behaviors=args.process_behaviors,
          data_dir_train="dataset/train/", data_dir_valid="dataset/valid/",
